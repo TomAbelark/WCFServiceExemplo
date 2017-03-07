96b15f9c8f43ccc478c7b23de8e5ea6436a04789
tom sabbath <tom.sabbath@hotmail.com>
2017-03-07 03:27:58 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsTesteWebService/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsTesteWebService
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServicoSomar.ServiceClient soma = new ServicoSomar.ServiceClient();
+            textBox3.Text = soma.Somar(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text)); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form2 Fr2 = new Form2();
+            Fr2.Show();
+        }
+    }
+}
---
+++ WindowsFormsTesteWebService/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsTesteWebService
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            Form1 fr1 = new Form1();
+            fr1.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
